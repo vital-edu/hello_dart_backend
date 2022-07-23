2b2d84c2c1737db65955493f483021bd08bfdcd3
Eduardo Vital Alencar Cunha <vital@hey.com>
2022-07-23 12:57:37 -0300

    Use async instead of promise.

--- bin/backend.dart
+++ bin/backend.dart
@@ -2,10 +2,10 @@ import 'dart:async';
 import 'package:shelf/shelf_io.dart' as shelf;
 import 'package:shelf/shelf.dart';
 
-void main(List<String> arguments) {
-  final server = shelf.serve(_handler, '0.0.0.0', 8000);
+void main(List<String> arguments) async {
+  final server = await shelf.serve(_handler, '0.0.0.0', 8000);
 
-  server.then((_) => print('Server running on ${_.address.address}:${_.port}'));
+  print('Server running on ${server.address.address}:${server.port}');
 }
 
 FutureOr<Response> _handler(request) {
