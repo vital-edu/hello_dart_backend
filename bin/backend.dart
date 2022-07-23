cd0cc6f783aa2c77ee99118abd51ec496df50316
Eduardo Vital Alencar Cunha <vital@hey.com>
2022-07-23 13:15:17 -0300

    Create log middleware.

--- bin/backend.dart
+++ bin/backend.dart
@@ -3,9 +3,27 @@ import 'package:shelf/shelf_io.dart' as shelf;
 import 'package:shelf/shelf.dart';
 
 void main(List<String> arguments) async {
-  final server = await shelf.serve(_handler, '0.0.0.0', 8000);
+  final pipeline = Pipeline().addMiddleware(log());
+
+  final server = await shelf.serve(
+    pipeline.addHandler(_handler),
+    '0.0.0.0',
+    8000,
+  );
 
   print('Server running on ${server.address.address}:${server.port}');
+}
+
+Middleware log() {
+  return (Handler handler) {
+    return (Request request) async {
+      print('Requested: ${request.method} ${request.url}');
+      final response = await handler(request);
+      print('Responded: ${response.statusCode}');
+
+      return response;
+    };
+  };
 }
 
 FutureOr<Response> _handler(request) {
