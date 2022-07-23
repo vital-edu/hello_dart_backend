6b03151668676662e21d1d76204a8c8413937354
Eduardo Vital Alencar Cunha <vital@hey.com>
2022-07-23 12:56:32 -0300

    Start server.

--- bin/backend.dart
+++ bin/backend.dart
@@ -1,5 +1,14 @@
-import 'package:backend/backend.dart' as backend;
+import 'dart:async';
+import 'package:shelf/shelf_io.dart' as shelf;
+import 'package:shelf/shelf.dart';
 
 void main(List<String> arguments) {
-  print('Hello world: ${backend.calculate()}!');
+  final server = shelf.serve(_handler, '0.0.0.0', 8000);
+
+  server.then((_) => print('Server running on ${_.address.address}:${_.port}'));
+}
+
+FutureOr<Response> _handler(request) {
+  print(request);
+  return Response(200, body: 'Hello, world!');
 }
