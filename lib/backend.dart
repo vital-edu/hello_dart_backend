0616e912c9220e35ab22a00c98ff05ff1674340f
Eduardo Vital Alencar Cunha <vital@hey.com>
2022-07-24 16:16:22 -0300

    Add application/json in response headers.

--- lib/backend.dart
+++ lib/backend.dart
@@ -5,7 +5,19 @@ import 'package:shelf/shelf.dart';
 import 'package:shelf_modular/shelf_modular.dart';
 
 Future<Handler> startShelfModular() async {
-  return Modular(module: AppModule(), middlewares: [
-    logRequests(),
-  ]);
+  return Modular(
+      module: AppModule(),
+      middlewares: [logRequests(), _addJsonContentTypeInResponseHeaders()]);
+}
+
+Middleware _addJsonContentTypeInResponseHeaders() {
+  return (Handler handler) {
+    return (Request request) async {
+      final response = await handler(request);
+      return response.change(headers: {
+        'content-type': 'application/json',
+        ...response.headers,
+      });
+    };
+  };
 }
